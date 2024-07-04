706aedf4384973984c25191b90ae66bcd90b6401
Cornelius Wefelscheid <cw@pariserplatz.eu>
2024-07-04 07:55:47 +0200

    remove GPL header from individual files

--- Generator/genOptStruct.py
+++ Generator/genOptStruct.py
@@ -5,18 +5,7 @@
  *
  *   This file is part of OpenOF.
  *
- *   OpenOF is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
 
- *   OpenOF is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
-
- *   You should have received a copy of the GNU General Public License
- *   along with OpenOF.  If not, see <http://www.gnu.org/licenses/>.
 
 
 This file contains all methods to generate source code from your model designed with sympy.
